d9e042a63b39318a15e53735da4d110470ee970e
LimNorman <Norman.lim26@ycdsbk12.ca>
2024-12-19 09:12:44 -0500

    Create FinalCPTNorman.java
    Basic Skeleton

---
+++ FinalCPTNorman.java
@@ -0,0 +1,13 @@
+import arc.*;
+
+public class FinalCPTNorman{
+	public static void main(String[] args){
+		Console con = new Console();
+		
+		//Variables
+		
+		
+		con.println("Hello World");
+		
+	}
+}
