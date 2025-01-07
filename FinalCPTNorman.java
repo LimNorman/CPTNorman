c40ef0400e71aa0dbdaeadc877654d9bd6818c1a
LimNorman <Norman.lim26@ycdsbk12.ca>
2025-01-07 09:28:59 -0500

    Created files for the highscores, themes and words in each theme. As well as opening each file

--- FinalCPTNorman.java
+++ FinalCPTNorman.java
@@ -4,6 +4,11 @@ public class FinalCPTNorman{
 	public static void main(String[] args){
 		Console con = new Console();
 		
+		//Opens Files
+	    TextOutputFile Score = new TextOutputFile("HighScore.txt", true);
+	    TextInputFile Theme = new TextInputFile("Themes.txt");
+	    TextInputFile Words = new TextInputFile("Words.txt");
+	    
 		//Variables
 		
 		
